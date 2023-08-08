95a7c2183b8f77b64e9e00eee39bea1c564c6711
raartist <rishabhzee98@gmail.com>
2023-08-08 13:45:50 +0530

    multiple design objects

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 
 //Styling in React -- css not allowed can be styled with the help of javascript properties like camelCase propeties e.g. backgroundColor instead backgroud-color
-
+/*/last property will take advantage of style in the styling array}*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>StyleSheet API</Text>
+      <View style={[styles.lightblue, styles.box]}>
+        <Text>Lightblue box</Text>
+      </View>
+      <View style={[styles.box, styles.lightgreen]}>
+        <Text>Lightgreen box</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +21,16 @@ const styles = StyleSheet.create({
     backgroundColor: "plum",
     padding: 60,
   },
-  title: {},
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    backgroundColor:'pink'
+  },
+  lightblue: {
+    backgroundColor: "lightblue",
+  },
+  lightgreen: {
+    backgroundColor: "lightgreen",
+  },
 });
