3dd08d08a326a987f381e4817c58b760fd3dc427
raartist <rishabhzee98@gmail.com>
2023-08-08 13:34:36 +0530

    styling using styleSheet api from reactnative

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 //Styling in React -- css not allowed can be styled with the help of javascript properties like camelCase propeties e.g. backgroundColor instead backgroud-color
 
 export default function App() {
   return (
-    <View style={{flex:1, backgroundColor:'plum', padding:60 }}>
-   <Text>StyleSheet API</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>StyleSheet API</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    padding: 60,
+  },
+  title: {},
+});
