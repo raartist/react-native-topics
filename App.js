4c75ece56075de5b3124b4560c72c702a93098ca
raartist <rishabhzee98@gmail.com>
2023-08-09 21:28:41 +0530

    flex direction

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Box from "./components/Box";
 
 /*/last property will take advantage of style in the styling array}*/
 
-// 27 - Flex
+// 27 - Flex-Direction
 
 // box shadow like box shadow object can only be applicable for ios devices not on android
 // to do box shadow on android devices we need to use android elevate api
@@ -14,7 +14,7 @@ export default function App() {
       <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156", flex:1 }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
       <Box style={{ backgroundColor: "#8e9b00" }}>Box 7</Box>
@@ -23,5 +23,11 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: { marginTop: 64, borderWidth: 6, borderColor: "pink", flex:1 },
+  container: {
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "pink",
+    flex: 1,
+    flexDirection: "column",
+  },
 });
