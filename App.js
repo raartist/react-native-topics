f4ac1e0cf3fca6b4168929b0a809128f1dbd91eb
raartist <rishabhzee98@gmail.com>
2023-08-15 17:32:52 +0530

    alignItems

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Box from "./components/Box";
 
 /*/last property will take advantage of style in the styling array}*/
 
-// 27 - Flex-justifyContent
+// 27 - Flex-alignItems
 
 // box shadow like box shadow object can only be applicable for ios devices not on android
 // to do box shadow on android devices we need to use android elevate api
@@ -11,13 +11,13 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#8e9b00", paddingVertical: 100 }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      {/* <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 7</Box>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 7</Box> */}
     </View>
   );
 }
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
     borderWidth: 6,
     borderColor: "pink",
     flex: 1,
-    flexDirection: "column",
-    justifyContent:"space-evenly"
+    flexDirection: "row",
+    alignItems: "flex-end",
   },
 });
