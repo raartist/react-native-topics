e75612d79adf09dc74ab644d25680a9836a41b90
raartist <rishabhzee98@gmail.com>
2023-08-16 10:28:46 +0530

    align content

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Box from "./components/Box";
 
 /*/last property will take advantage of style in the styling array}*/
 
-// 32 - Flex-flexWrap
+// 33 - Flex-alignContent
 
 // box shadow like box shadow object can only be applicable for ios devices not on android
 // to do box shadow on android devices we need to use android elevate api
@@ -11,13 +11,13 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f"}}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56"}}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156"}}>Box 4</Box>
-      <Box style={{ backgroundColor: "#6b0803"}}>Box 5</Box> 
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 7</Box>
+      <Box style={[{ backgroundColor: "#8e9b00" }, styles.box]}>Box 1</Box>
+      <Box style={[{ backgroundColor: "#b65d1f" }, styles.box]}>Box 2</Box>
+      <Box style={[{ backgroundColor: "#1c4c56" }, styles.box]}>Box 3</Box>
+      <Box style={[{ backgroundColor: "#ab9156" }, styles.box]}>Box 4</Box>
+      <Box style={[{ backgroundColor: "#6b0803" }, styles.box]}>Box 5</Box>
+      <Box style={[{ backgroundColor: "#1c4c56" }, styles.box]}>Box 6</Box>
+      <Box style={[{ backgroundColor: "#8e9b00" }, styles.box]}>Box 7</Box>
     </View>
   );
 }
@@ -25,11 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     marginTop: 64,
-    height:300,
+    height: 300,
     borderWidth: 6,
     borderColor: "pink",
-    flexWrap:"wrap",
-    flexDirection:"row",
-    flex:1
+    flexWrap: "wrap",
+
+    alignContent: "space-between",
+  },
+  box: {
+    width: 100,
+    height: 50,
   },
 });
