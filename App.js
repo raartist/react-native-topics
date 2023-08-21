6d3365c5e8122df033c987c4e1aa14ffa3e3981e
raartist <rishabhzee98@gmail.com>
2023-08-21 20:53:53 +0530

    setup for dynamic ui

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import { StyleSheet, View } from "react-native";
-import Box from "./components/Box";
+import { StyleSheet, Text, View } from "react-native";
 
 /*/last property will take advantage of style in the styling array}*/
 
-// 37 - Flex-Grow
+// 39- Dynamic user interfaces
 
 // Flex:<positive number>
 // flexGrow: '<positive number>', flexShrink:1, flexBasis:0
@@ -14,13 +13,7 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={[{ backgroundColor: "#8e9b00", flexGrow: 1 }, styles.box]}>Box 1 </Box>
-      <Box style={[{ backgroundColor: "#b65d1f" }, styles.box]}>Box 2 </Box>
-      <Box style={[{ backgroundColor: "#1c4c56" }, styles.box]}>Box 3</Box>
-      <Box style={[{ backgroundColor: "#ab9156" }, styles.box]}>Box 4</Box>
-      <Box style={[{ backgroundColor: "#6b0803" }, styles.box]}>Box 5</Box>
-      <Box style={[{ backgroundColor: "#1c4c56" }, styles.box]}>Box 6</Box>
-      <Box style={[{ backgroundColor: "#8e9b00" }, styles.box]}>Box 7</Box>
+      <Text>Dynamic ui</Text>
     </View>
   );
 }
@@ -28,17 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     marginTop: 64,
-    borderWidth: 6,
-    borderColor: "pink",
-    flexWrap: "wrap", 
-    columnGap: 20,
-    rowGap: 12,
-    gap: 23,
-    height:"100%",
-  },
-  box: {
-    width: "100%",
-    height: 50,
-    flexGrow:1
+    backgroundColor:"plum",
+    height:"100%"
   },
 });
