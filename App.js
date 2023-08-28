d47a06884adb58ea83b04ff36102ba10c8a5e2fd
raartist <rishabhzee98@gmail.com>
2023-08-29 00:10:12 +0530

    safe area view

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 
-import { StyleSheet, Text, useWindowDimensions, View } from "react-native";
+import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 /*/last property will take advantage of style in the styling array}*/
 
-// 42 - useWindowDimensions
+// 43 - SafeAreaView
+//safe area view doesn't work in all the android devices that's why we need to do some jugad with the help of platform.os tool
+// and apply padding top for android devices only.. because safeArea view works fine for ios devices
 
-// (Dimensions API drawbacks) it will failed to meet our dimension conditions when screen is rotated | thus we need to reload our app to recalculate the dimensions.
-// for resolving this issue we are using useState and useEffect hooks.
-// although this issue can also be solved with useWindowDimensions Api from react-native.. without using any hook
 // Flex:<positive number>
 // flexGrow: '<positive number>', flexShrink:1, flexBasis:0
 
@@ -15,16 +14,16 @@ import { StyleSheet, Text, useWindowDimensions, View } from "react-native";
 // to do box shadow on android devices we need to use android elevate api
 
 export default function App() {
-  const windowWidth = useWindowDimensions.width;
-  const windowHeight = useWindowDimensions.height;
+
   
   return (
+    <SafeAreaView style={styles.safeContainer}>
     <View style={styles.container}>
-      <View style={[styles.box,{width: windowWidth > 500 ? "70%" : "90%",
-     height: windowHeight > 500 ? "60%" : "40%",}]}>
-        <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>Dynamic ui</Text>
+      <View style={styles.box}>
+        <Text style={styles.text} >welcome!</Text>
       </View>
     </View>
+    </SafeAreaView>
   );
 }
 
@@ -33,21 +32,23 @@ export default function App() {
 
 
 const styles = StyleSheet.create({
+  safeContainer:{
+    flex:1,
+    paddingTop: Platform.OS === 'android' ? 25 : 0
+  },
   container: {
-    marginTop: 64,
     backgroundColor: "plum",
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
   box: {
     // width: windowWidth > 500 ? "70%" : "90%",
     // height: windowHeight > 500 ? "60%" : "40%",
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
+    // backgroundColor: "lightblue",
+    padding:20,    
   },
   text: {
     // fontSize: windowWidth > 500 ? 28 : 24,
+    fontWeight:"bold",
+    textAlign:"center"
   },
 });
