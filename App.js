02ab8a47eda6b58d0034d060d074a5481a58d9d8
raartist <rishabhzee98@gmail.com>
2023-08-30 18:21:34 +0530

    exercise1 - pokemon card -p1

--- App.js
+++ App.js
@@ -1,55 +1,23 @@
+import React from 'react'
+import { Platform, SafeAreaView, StyleSheet, Text } from 'react-native'
+import PokemonCard from './components/PokemonCard'
 
-import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
+//45 - exercise 1 - creating pokemon cards
 
-/*/last property will take advantage of style in the styling array}*/
-
-// 44 - platform specific code
-// we already did this in last section where we added padding top for android in a different way with the help of 
-// platform package from react-native
-//because our safeAreaView was only working for ios devices.
-
-// Flex:<positive number>
-// flexGrow: '<positive number>', flexShrink:1, flexBasis:0
-
-// box shadow like box shadow object can only be applicable for ios devices not on android
-// to do box shadow on android devices we need to use android elevate api
-
-export default function App() {
-
-  
+const App = () => {
   return (
-    <SafeAreaView style={styles.safeContainer}>
-    <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text} >welcome!</Text>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>App</Text>
+      <PokemonCard/>
     </SafeAreaView>
-  );
+  )
 }
 
-// const windowWidth = dimensions.get("window").width;
-// const windowHeight = dimensions.get("window").height;
-
-
 const styles = StyleSheet.create({
-  safeContainer:{
+  container:{
     flex:1,
-    paddingTop: Platform.OS === 'android' ? 25 : 0
+    backgroundColor:"#f5f5f5",
+    marginTop: Platform.OS === "android"?30:0
   },
-  container: {
-    backgroundColor: "plum",
-    flex: 1,
-  },
-  box: {
-    // width: windowWidth > 500 ? "70%" : "90%",
-    // height: windowHeight > 500 ? "60%" : "40%",
-    // backgroundColor: "lightblue",
-    padding:20,    
-  },
-  text: {
-    // fontSize: windowWidth > 500 ? 28 : 24,
-    fontWeight:"bold",
-    textAlign:"center"
-  },
-});
+})
+export default App
---
+++ components/PokemonCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const PokemonCard = () => {
+  return (
+    <View>
+      <Text>PokemonCard</Text>
+    </View>
+  )
+}
+
+export default PokemonCard
+
+const styles = StyleSheet.create({
+    
+})
